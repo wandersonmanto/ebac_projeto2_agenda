1f2e940edc9ff0ff46f98d9b90cde90f664408dd
Wanderson Barbosa <wandersonmanto@gmail.com>
2023-02-15 20:50:16 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,36 @@
+const form = document.getElementById('form-contato');
+const inputNome = document.getElementById('nome-contato');
+const inputTelefone = document.getElementById('telefone-contato');
+
+let linhas = '';
+
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
+
+  handleNewContato();
+
+})
+
+function handleNewContato() {
+  const inputNome = document.getElementById('nome-contato');
+  const inputTelefone = document.getElementById('telefone-contato');
+
+  let linha = '<tr>';
+  linha += `<td>${inputNome.value}</td>`;
+  linha += `<td>${inputTelefone.value}</td>`;
+  linha += '</tr>';
+
+  linhas += linha;
+
+  inputNome.value = '';
+  inputTelefone.value = '';
+
+  atualizarTabela();
+
+
+}
+
+function atualizarTabela() {
+  const corpoTabela = document.querySelector('tbody');
+  corpoTabela.innerHTML = linhas;
+}
